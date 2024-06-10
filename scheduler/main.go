65bc7057d3de263e18b292c93137e38ed6713254
Copito <6876182+copito@users.noreply.github.com>
2024-06-09 20:52:55 -0500

    :test_tube: manual testing project

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/copito/gocronscheduler/scheduler"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
-	fmt.Println("Hello")
+	ctx := context.Background()
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Error creating Kubernetes client: %s", err.Error())
+	}
+
+	// Start Watcher
+	go scheduler.WatchCronJobs(ctx, clientSet)
+
+	// Create schedule
+	jobName := "example-cronjob"
+	schedule := "*/5 * * * *"
+	containerImage := "busybox"
+
+	cronJob := scheduler.GenerateCronJobDefinition(jobName, containerImage, schedule)
+
+	err = scheduler.CreateCronJob(ctx, clientSet, cronJob)
+	if err != nil {
+		log.Fatalf("Error creating cron job: %s", err.Error())
+	}
+
+	fmt.Println("Cron job created successfully")
+
+	select {}
 }
